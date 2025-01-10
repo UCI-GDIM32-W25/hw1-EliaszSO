58df9f9fab8a0ecc35be02b97b9abe3abdf7065c
ElijahSO <elismuga@gmail.com>
2025-01-10 07:56:51 -0800

    Player.cs MoveFromAxis translates player with event axis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,4 +25,22 @@ public class Player : MonoBehaviour
     {
         
     }
+
+    
+    private void MoveFromAxis()
+    {
+        // i like to use the axis floats for movement becayse they let me use the arrow keys 
+        // and WASD with less code overall
+
+        // get axis float values from unity event system
+        float horizontalFloat = Input.GetAxis("Horizontal");
+        float verticalFloat = Input.GetAxis("Vertical");
+
+        // make a vector 2 from the floats denoting the move direction
+        Vector2 moveDirection = new Vector2(verticalFloat, horizontalFloat);
+
+        //transform the player by the move direction * speed fixed with deltatime
+        _playerTransform.Translate(moveDirection * _speed * Time.deltaTime);
+
+    }
 }
