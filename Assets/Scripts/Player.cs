424cd603422612493276ea49ff4a5e4ab99ccda7
ElijahSO <elismuga@gmail.com>
2025-01-10 08:00:55 -0800

    MoveFrom axis moves in correct direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,7 +18,8 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        // move the player
+        MoveFromAxis();
     }
 
     public void PlantSeed ()
@@ -33,11 +34,11 @@ public class Player : MonoBehaviour
         // and WASD with less code overall
 
         // get axis float values from unity event system
-        float horizontalFloat = Input.GetAxis("Horizontal");
-        float verticalFloat = Input.GetAxis("Vertical");
+        float horizontalFloat = Input.GetAxisRaw("Horizontal");
+        float verticalFloat = Input.GetAxisRaw("Vertical");
 
         // make a vector 2 from the floats denoting the move direction
-        Vector2 moveDirection = new Vector2(verticalFloat, horizontalFloat);
+        Vector2 moveDirection = new Vector2(horizontalFloat, verticalFloat);
 
         //transform the player by the move direction * speed fixed with deltatime
         _playerTransform.Translate(moveDirection * _speed * Time.deltaTime);
