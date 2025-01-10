57844b65a11874479d97ce865809572a1c66f02c
ElijahSO <elismuga@gmail.com>
2025-01-10 12:19:49 -0800

    started animating things
    made a growing animation and an idle animation
    also added a plants script for future use

---
+++ Assets/Scripts/Plants.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plants : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // swap between plants?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // could run grow here
+        // make seeds fall off when its fully grown
+    }
+}
