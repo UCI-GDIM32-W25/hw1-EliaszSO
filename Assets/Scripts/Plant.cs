4354c2f4adf3364535c781e8437398bd3b52910e
ElijahSO <elismuga@gmail.com>
2025-01-11 15:38:12 -0800

    plants grow in a fun way now

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    private float _tickTimer;
+    [SerializeField] private float _ticksPerSecond = 2;
+    private float _timePerTick;
+    
+    // the probability the plant will grow every tick
+    [SerializeField] private float _growChance;
+    
+    private Animator _plantAnimator;
+
+    private int _growthStage;
+    
+
+    void Start()
+    {
+        //setup timer and tick values
+        _tickTimer = 0f;
+        _timePerTick = 1 / _ticksPerSecond;
+
+        //get animator component
+        _plantAnimator = GetComponent<Animator>();
+
+        _growthStage = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // could run grow here
+        // make seeds fall off when its fully grown
+
+        //growth by tick system:
+        //increase timer by the amount of time that has passed
+        _tickTimer += Time.deltaTime;
+
+        // if timer is greater than time per tick a tick should have been triggered
+        if(_tickTimer >= _timePerTick)
+        {
+            TickEvent();
+            
+            //reset timer (discounting error amount)
+            _tickTimer -= _timePerTick;
+            
+        }
+    }
+
+    /// <summary>
+    /// triggered on ever tick for this game object
+    /// </summary>
+    private void TickEvent()
+    {
+        //try to grow the plant
+        float randomValue = Random.Range(0f, 1f);
+        
+        if (randomValue < _growChance)
+        {
+            GrowPlant();
+        }
+
+    }
+
+    public void GrowPlant()
+    {
+        // increase the growth stage
+        _growthStage +=1;
+
+        // cause the animation to change
+        _plantAnimator.SetInteger("GrowthLevel", _growthStage);
+    }
+}
